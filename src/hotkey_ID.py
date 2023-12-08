a1eecab953eb27bfb06c5818982572d7273918b1
evlar <evlargit@proton.me>
2023-12-07 19:28:28 -0700

    fix accidental script replacement

--- src/hotkey_ID.py
+++ src/hotkey_ID.py
@@ -1,180 +1,64 @@
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from matplotlib.ticker import FormatStrFormatter
 import pandas as pd
 import os
 
-def load_hotkey_names(file_path):
-    if os.path.exists(file_path):  
-        return pd.read_csv(file_path).set_index('hotkey').to_dict()['hotkey_name']
-    return {}
+def load_hotkeys_from_snapshot(log_file):
+    if os.path.exists(log_file):
+        df = pd.read_csv(log_file)
+        return set(df['hotkey'].unique())
+    else:
+        print(f"Log file {log_file} not found.")
+        return set()
 
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import pandas as pd
-import os
+def load_existing_hotkeys(file_path):
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path)
+    else:
+        return pd.DataFrame(columns=['hotkey', 'hotkey_name'])
 
+def display_existing_hotkeys(df):
+    if not df.empty:
+        sorted_df = df.sort_values(by='hotkey_name')
+        print("Current Hotkeys and Their Names:")
+        print(sorted_df)
+    else:
+        print("No existing hotkeys found.")
 
-def plot_combined_hotkeys(df, variable, hotkey_names):
-    plt.figure(figsize=(15, 8))
-    
-    # Ensure the variable is numeric, stripping 'τ' if the variable is 'stake'
-    if variable == 'stake':
-        df[variable] = df[variable].str.replace('τ', '').astype(float)
-    
-    # Convert timestamps to datetime and sort
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df.sort_values('timestamp', inplace=True)
-
-    # Get unique hotkeys and sort by their names
-    unique_hotkeys = df['hotkey'].unique()
-    sorted_hotkeys = sorted(unique_hotkeys, key=lambda x: hotkey_names.get(x, 'Unknown'))
-
-    # Plot each hotkey's data
-    for hotkey in sorted_hotkeys:
-        hotkey_data = df[df['hotkey'] == hotkey]
-        if not hotkey_data.empty:
-            plt.plot(hotkey_data['timestamp'], hotkey_data[variable], label=hotkey_names.get(hotkey, hotkey), marker='o', linestyle='-')
-
-    # Formatting the plot
-    plt.title(f'{variable.capitalize()} over time for all hotkeys')
-    plt.xlabel('Time')
-    plt.ylabel(variable.capitalize())
-    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-    plt.xticks(rotation=45)
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_variable(filename, variable, hotkey):
-    df = pd.read_csv(filename)
-    df['timestamp'] = pd.to_datetime(df['timestamp'])  
-    df_filtered = df[df['hotkey'] == hotkey]
-    
-    plt.figure(figsize=(10, 6))
-    plt.plot(df_filtered['timestamp'], df_filtered[variable], marker='o', markersize=3)
-    plt.title(f'{variable} over time for {hotkey}')
-    plt.xlabel('Time')
-    plt.ylabel(variable)
-    
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.grid(True)
-    plt.show()
-
-def select_hotkey(df, hotkey_names):
-    unique_hotkeys = df['hotkey'].unique()
-    hotkeys_with_names = [(key, hotkey_names.get(key, 'Unknown')) for key in unique_hotkeys]
-    hotkeys_with_names.sort(key=lambda x: x[1])  # Sort by hotkey name
-
-    print("Available hotkeys:")
-    for idx, (key, name) in enumerate(hotkeys_with_names, 1):
-        print(f"{idx}. {key} ({name})")
-    choice = input("Select a hotkey (number) or type 'exit' to quit: ")
-    if choice.lower() == 'exit':
-        return None
-    try:
-        selected_index = int(choice) - 1
-        if 0 <= selected_index < len(unique_hotkeys):
-            return hotkeys_with_names[selected_index][0]
+def rename_hotkeys(df):
+    edit = input("Would you like to edit a hotkey name? (yes/no): ").strip().lower()
+    while edit == 'yes':
+        hotkey_to_edit = input("Enter the hotkey you want to rename: ").strip()
+        if hotkey_to_edit in df['hotkey'].values:
+            new_name = input(f"Enter a new name for hotkey {hotkey_to_edit}: ").strip()
+            if new_name:
+                df.loc[df['hotkey'] == hotkey_to_edit, 'hotkey_name'] = new_name
         else:
-            print("Invalid selection. Please try again.")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-    return None
+            print("Hotkey not found. Please enter a valid hotkey.")
+
+        edit = input("Would you like to edit another hotkey name? (yes/no): ").strip().lower()
+
+    return df
 
 def main():
-    # Get the directory where charts.py is located
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    # Construct the full paths to the data files
-    filename = os.path.join(current_dir, 'hotkeys.log')
-    hotkey_names_file = os.path.join(current_dir, 'hotkey_names.csv')
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    log_file_path = os.path.join(script_dir, 'hotkeys.log')
+    hotkey_file_path = os.path.join(script_dir, 'hotkey_names.csv')
 
-    # Load the hotkey names
-    if not os.path.exists(hotkey_names_file):
-        print(f"Error: The file {hotkey_names_file} was not found.")
-        return  # Exit the function if the file is not found
-    hotkey_names = load_hotkey_names(hotkey_names_file)
+    unique_hotkeys = load_hotkeys_from_snapshot(log_file_path)
+    hotkey_df = load_existing_hotkeys(hotkey_file_path)
 
-    # Load the data
-    if not os.path.exists(filename):
-        print(f"Error: The file {filename} was not found.")
-        return  # Exit the function if the file is not found
-    df = pd.read_csv(filename)
-    df['timestamp'] = pd.to_datetime(df['timestamp'])  
+    # Display existing hotkeys if the file exists, sorted by hotkey_name
+    display_existing_hotkeys(hotkey_df)
 
-    while True:  # Main loop
-        print("\nSelect an action:")
-        print("1. Plot variable for a single hotkey")
-        print("2. Plot all hotkeys")
-        print("3. Return to main menu")
-        main_choice = input("Enter your choice: ")
+    existing_hotkeys = set(hotkey_df['hotkey'])
+    new_hotkeys = unique_hotkeys - existing_hotkeys
+    for hotkey in new_hotkeys:
+        name = input(f"Enter a name for new hotkey {hotkey} (press Enter to skip): ").strip() or hotkey
+        hotkey_df = hotkey_df._append({'hotkey': hotkey, 'hotkey_name': name}, ignore_index=True)
 
-        if main_choice == '1':
-            hotkey = select_hotkey(df, hotkey_names)
-            if hotkey:
-                while True:  
-                    print("\nSelect the variable to visualize:")
-                    print("1. Stake")
-                    print("2. Trust")
-                    print("3. Consensus")
-                    print("4. Incentive")
-                    print("5. Emission")
-                    print("6. Return to previous menu")
-                    choice = input("Enter your choice: ")
+    hotkey_df = rename_hotkeys(hotkey_df)
+    hotkey_df.to_csv(hotkey_file_path, index=False)
 
-                    if choice == '1':
-                        plot_variable(filename, 'stake', hotkey)
-                    elif choice == '2':
-                        plot_variable(filename, 'trust', hotkey)
-                    elif choice == '3':
-                        plot_variable(filename, 'consensus', hotkey)
-                    elif choice == '4':
-                        plot_variable(filename, 'incentive', hotkey)
-                    elif choice == '5':
-                        plot_variable(filename, 'emission', hotkey)
-                    elif choice == '6':
-                        break  
-                    else:
-                        print("Invalid choice. Please try again.")
-
-        elif main_choice == '2':
-            print("\nSelect the variable for comparison across all hotkeys:")
-            print("1. Stake")
-            print("2. Trust")
-            print("3. Consensus")
-            print("4. Incentive")
-            print("5. Emission")
-            print("6. Return to previous menu")
-            comp_choice = input("Enter your choice: ")
-            variable = None
-
-            if comp_choice == '1':
-                variable = 'stake'
-            elif comp_choice == '2':
-                variable = 'trust'
-            elif comp_choice == '3':
-                variable = 'consensus'
-            elif comp_choice == '4':
-                variable = 'incentive'
-            elif comp_choice == '5':
-                variable = 'emission'
-    
-            if variable:
-                plot_combined_hotkeys(df, variable, hotkey_names)
-            else:
-                print("Invalid choice. Please try again.")
-
-        elif main_choice == '6':
-            break  
-        else:
-            print("Invalid choice. Please enter a number between 1 and 6.")
+    print(f"Updated hotkey names saved to {hotkey_file_path}")
 
 if __name__ == "__main__":
     main()
